4f52f49aa82772bc8a941fb9b0877c3e7b7a3462
Austen Talbot <austentalbot@gmail.com>
2014-04-25 15:05:03 -0400

    Set up basic recursive framework. Began implementation of array and string stringification.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,33 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // recursively call stringifyJSON to pull each element out
+  // handle different types (obj, array, string, number) separately first, then call stringifyJSON recursively
+
+  // populate empty variables
+  var strung='';
+  var i;
+
+  //ARRAY
+  if (Array.isArray(obj)) {
+  	strung='[';
+  	for (i=0; i<obj.length; i++) {
+  		strung+=stringifyJSON(obj[i]);
+  		i<obj.length-1 ? strung+=', ' : strung+=']';
+  	}
+  };
+
+  //STRING
+  if (typeof obj === 'string') {
+  	strung='"';
+  	strung+=obj;
+  	strung+='"'
+  }
+
+  //NUMBER
+
+  //OBJECT
+
+  return strung;
+
 };
