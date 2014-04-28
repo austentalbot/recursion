0a00ef54311ad722b0a3a196830bee3fa96ba206
Austen Talbot <austentalbot@gmail.com>
2014-04-27 22:14:32 -0400

    Fixed indenting, added comments, adjusted minor errors. All is working now. Finished getElementsByClassName.js, mandatory recursion assignment, and mandatory HR pre-course curriculum!

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,29 +4,34 @@
 // };
 
 // But instead we're going to implement it from scratch:
+
 var getElementsByClassName = function(className){
   //loop over all nodes at one level
   //test to see if class name in node
   //recursively loop over all child nodes
 
   var elements=[];
-  var root=document.body;
-  var i;
 
-  // create function to be recursively called on children
-  var getElements=function(level) {
-  	if (level.hasChildNodes) {
-  	  children=level.childNodes;
-	  for (i=0; i<children.length; i++) {
-		if (children[i].classList & children[i].classList.contains(className)) {
-		  elements.push(children[i]);
-		}
-		getElements(children[i]);
-	  }
-	}
-  };
+  //create function to be recursively called on children
+	var getElements = function (level) {
+		//check if class in level
+    if (level.classList) {
+      if (level.classList.contains(className)) {
+        elements.push(level);
+      }
+    }
+    //recurse over children
+    if (level.hasChildNodes) {
+      for (var i =0; i < level.childNodes.length; i++) {
+        getElements(level.childNodes[i]);
+      }
+    }
+	};
 
-  getElements(root);
-  return elements;
+	// call getElements function to start recursion
+	getElements(document.body);
+
+	return elements;
 
 };
+
