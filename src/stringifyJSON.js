d95d524a74ccd81a2f411a333b12fb526428d71a
Austen Talbot <austentalbot@gmail.com>
2014-05-04 18:36:39 -0400

    Reviewed, cleaned up, and refactored stringifyJSON.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -8,35 +8,35 @@ var stringifyJSON = function(obj) {
 
   // populate empty variables
   var strung='';
-  var i;
 
   if (Array.isArray(obj)) { //ARRAY
   	strung='[';
-  	for (i=0; i<obj.length; i++) {
-  		strung+=stringifyJSON(obj[i]);
-  		i<obj.length-1 ? strung+=',' : strung+=']';
-  	}
+    _.each(obj, function(val, ind) {
+  		strung+=stringifyJSON(val);
+  		ind<obj.length-1 ? strung+=',' : strung+=']';
+  	});
   	// case for empty array
   	obj.length===0 ? strung+=']' : null;
   } else if (typeof obj === 'string') { //STRING
   	strung='"';
   	strung+=obj;
-  	strung+='"'
+  	strung+='"';
   } else if (typeof obj === 'number' | typeof obj === 'boolean' | obj === null) { //NUMBER AND BOOLEAN AND NULL
   	strung+=obj;
   } else if (typeof obj === 'object') { //OBJECT
-  	strung='{'
-  	for (i in obj) {
+  	strung='{';
+    _.each(obj, function(val, ind) {
   		//HANDLE FUNCTIONS AND UNDEFINED
-  		if (typeof obj[i]!== 'function' & typeof obj[i]!== 'undefined') {  		strung+=stringifyJSON(i);
-	  		strung+=':'
-	  		strung+=stringifyJSON(obj[i]);
+  		if (typeof val!== 'function' & typeof val!== 'undefined') {  		
+        strung+=stringifyJSON(ind);
+	  		strung+=':';
+	  		strung+=stringifyJSON(val);
 	  		strung+=',';
   		} else { //functions and undefined should return '{}'
   			strung='{ ';
   		}
-  	}
-  	strung=strung.slice(0, strung.length-1)+'}'
+  	});
+  	strung=strung.slice(0, strung.length-1)+'}';
   	// handle empty object
   	if (!Object.getOwnPropertyNames(obj).length) {
   		strung='{}';
